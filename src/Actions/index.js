3f45a6ccaac2188531406077890488de175e1844
YudaiNoda <yudainoda@YudaiNodanoMacBook-Air.local>
2021-10-04 00:45:47 +0900

    redux-action

---
+++ src/Actions/index.js
@@ -0,0 +1,13 @@
+// ActionCreater 
+// Componentで使用される
+
+export const INCREMENT = "INCREMENT"
+export const DECREMENT = "DECREMENT"
+
+export const increment = () => ({
+    type: INCREMENT
+})
+
+export const decrement = () => ({
+    type: DECREMENT
+})
