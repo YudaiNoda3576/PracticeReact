2518dc75db2e7ab56acc427c074f0b482de31c17
YudaiNoda <yudainoda@YudaiNodanoMacBook-Air.local>
2021-10-17 22:29:07 +0900

    画面遷移の実装

--- src/index.js
+++ src/index.js
@@ -6,17 +6,21 @@ import './index.css';
 import EventsIndex from './Components/events_index';
 import reducer from './Reducers';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import thunk from 'redux-thunk';
+import EventsNew from './Components/events_new' 
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
-      <EventsIndex />
-    </Provider>
-  </React.StrictMode>,
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/events/new" component={ EventsNew }/>
+          <Route exact path="/" component={ EventsIndex }/>
+        </Switch>
+      </BrowserRouter>
+    </Provider>,
   document.getElementById('root')
 );
 
